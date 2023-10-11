46f193c4e207a8b0a773a1c5626ad7a57d12d9c9
M Raihan Aryadevin <mraihanad.21@gmail.com>
2023-10-11 14:05:22 +0700

    menambahkan layout login dan regis

--- app/src/main/java/com/mympasi/LoginActivity.kt
+++ app/src/main/java/com/mympasi/LoginActivity.kt
@@ -2,10 +2,17 @@ package com.mympasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mympasi.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
     }
+
 }
